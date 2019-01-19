f76019e32258fcf521a761f22b0fd4aca23d3c10
HiromitsuShioya <hiroshica@gmail.com>
2019-01-19 15:28:35 +0900

    最低限テストする分のインターフェース実装

--- mucom88dll/mucom88dll.cpp
+++ mucom88dll/mucom88dll.cpp
@@ -3,4 +3,57 @@
 
 #include "stdafx.h"
 
+#include "mucom88dll.h"
 
+
+namespace Mucom88_DLL{
+
+	INT32 Mucom88Core::CreateVM() {
+		for (INT32 index = 0; index < kCreateMax; ++index) {
+			if (m_MucomVM[index] == NULL) {
+				m_MucomVM[index] = new CMucom();
+				m_LatestSelectNo = index;
+				return index;
+			}
+		}
+		return -1;
+	}
+
+
+	// index -1 = 最後に選択していたVM
+	bool Mucom88Core::RemoveVM(INT32 index) {
+		if (index == -1) {
+			for (index = kCreateMax; index >= 0; --index) {
+				if (m_MucomVM[index] != NULL) {
+					delete m_MucomVM[index];
+					m_MucomVM[index] = NULL;
+					return true;
+				}
+			}
+		}
+		else {
+			if (m_MucomVM[index] != NULL) {
+				delete m_MucomVM[index];
+				m_MucomVM[index] = NULL;
+				return true;
+			}
+		}
+		return false;
+	}
+	INT32 Mucom88Core::checkindex(INT32 index) {
+		if (index == -1) {
+			index = m_LatestSelectNo;
+		}
+		return index;
+	}
+
+	void Mucom88Core::Init(INT32 index, INT32 option) {
+		index = checkindex(index);
+		m_MucomVM[index]->Init(NULL, option, 0);
+	}
+	void Mucom88Core::Reset(INT32 index, INT32 option) {
+		INT32 gindex = checkindex(index);
+		m_MucomVM[index]->Reset(option);
+	}
+
+}
---
+++ mucom88dll/mucom88dll.h
@@ -0,0 +1,32 @@
+﻿// mucom88dll.cpp : DLL アプリケーション用にエクスポートされる関数を定義します。
+//
+#include "stdafx.h"
+
+#include "cmucom.h"
+#include "wavout.h"
+
+#define kCreateMax    (1)
+
+#ifdef MUCOM88DLL_EXPORTS
+#define DLLTEST_API __declspec(dllexport)
+#else
+#define DLLTEST_API __declspec(dllimport)
+#endif
+
+
+namespace Mucom88_DLL {
+	class Mucom88Core {
+		CMucom *m_MucomVM[kCreateMax];
+		INT32 m_LatestSelectNo;
+
+		DLLTEST_API INT32 CreateVM();   // 返り値 -1 = error : 0 > インデックス番号
+		// index -1 = 最後に選択していたVM
+		DLLTEST_API bool RemoveVM(INT32 index = -1);
+		DLLTEST_API void Init(INT32 index = -1, INT32 option = 0);
+		DLLTEST_API void Reset(INT32 index = -1, INT32 option = 0);
+
+
+		INT32 checkindex(INT32 index);
+
+	};
+}
